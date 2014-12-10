346db4e36cb11a18953da27c4fe837e716d19f46
hhlusine <lhovhannisyan@yandex.ru>
2014-12-10 21:00:43 +0400

    Added Piece.h file

--- TestChess/TestChess/TestChess.cpp
+++ TestChess/TestChess/TestChess.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	char color  = 'w';
+	cout << "Please select a color w/b ? ";
+	cin >> color;
 	return 0;
 }
 
